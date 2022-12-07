7f17e557baa7787b562c9da97d8b8ffbe0d2966d
IrulHaqq <96125255+IrulHaqq@users.noreply.github.com>
2022-12-07 17:33:34 +0700

    Add files via upload

---
+++ MODUL2_YUWANDI/MODUL2_YUWANDI/src/TransportasiAir.java
@@ -0,0 +1,23 @@
+public class TransportasiAir {
+
+    protected int jumlahKursi;
+    protected int biaya;
+
+    public TransportasiAir(int jumlahKursi, int biaya){
+        this.jumlahKursi = jumlahKursi;
+        this.biaya = biaya;
+    }
+
+    public void informasi(){
+        System.out.println("Transportasi air jenis");
+    }
+
+    public void berlayar(){
+        
+    }
+
+    public void berlabuh(){
+        
+    }
+
+}
